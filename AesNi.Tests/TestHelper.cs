ee282e376597c6f8e93b30d3d29c10f6ecbe2156
Nicolas Portmann <ndportmann@gmail.com>
2022-12-10 12:59:10 +0100

    Use built-in hex encoding

--- AesNi.Tests/TestHelper.cs
+++ AesNi.Tests/TestHelper.cs
@@ -7,12 +7,7 @@ namespace AesNi.Tests
         public static byte[] ToByteArray(this string hex)
         {
             if (hex.Length % 2 != 0) hex = "0" + hex;
-
-            var NumberChars = hex.Length;
-            var bytes = new byte[NumberChars / 2];
-            for (var i = 0; i < NumberChars; i += 2)
-                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-            return bytes;
+            return Convert.FromHexString(hex);
         }
     }
 }
